91e0622668c9ed89e929a2e7292bf9863674e7c8
SinisterXI <shameerawais32@gmail.com>
2024-05-05 19:08:37 +0500

    Database

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0328@localhost/DB
 db = SQLAlchemy(app)
 
 # User model
-class User(db.Model):
+class User(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
